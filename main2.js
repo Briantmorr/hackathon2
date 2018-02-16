100a55b1e4c50d0c306921252c264c71859f634a
Nolan <nemerson17@gmail.com>
2018-02-15 20:01:12 -0800

    Click handler and payment select

---
+++ main2.js
@@ -0,0 +1,19 @@
+$(document).ready(initializeApp);
+
+function initializeApp(){
+    $('.keypadContainer').on('click','.row button',keypadClick);
+}
+
+function keypadClick(){
+    if ()
+    var
+}
+
+if (paymentMethod === 'credit'){
+    $('.primaryDisplayContainer').text('Please enter zip code');
+} else if (paymentMethod === 'debit'){
+    $('.primaryDisplayContainer').text('Please enter pin');
+} else if (paymentMethod === 'cash'){
+    //go to next step
+}
+
