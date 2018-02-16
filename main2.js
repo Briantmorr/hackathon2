7c28200a8d85068c480c6617adcbf820dc4c9963
Nolan <nemerson17@gmail.com>
2018-02-15 20:56:59 -0800

    Split click handler into debit and credit options

--- main2.js
+++ main2.js
@@ -1,30 +1,47 @@
-$(document).ready(initializeApp);
+$(document).ready();
 
-function initializeApp(){
-    $('.keypadContainer').on('click','.row button',keypadClick);
-}
 
-var keyCode = '';
-function keypadClick(){
+var keyCodeDebit = '';
+function keypadDebit(){
     var lastKey = $(this).text();
     if (lastKey === 'Clear'){
-
-        //$('.keypadDisplay').text($('.keypadDisplay').text().splice($('.keypadDisplay').text().length-2),1);
+        $('.keypadDisplay').text($('.keypadDisplay').text().substring(0,$('.keypadDisplay').text().length-1));
     } else if (lastKey === 'Enter'){
-        if (keyCode.length >= 4){
+        if (keyCodeDebit.length === 4){
+            $('.keypadDisplay').text('');
             displayStage++;
             changeDisplay();
         }
     } else {
-        keyCode += $(this).text();
+        keyCodeDebit += $(this).text();
         $('.keypadDisplay').text($('.keypadDisplay').text()+'*');
-        console.log(keyCode);
+        console.log(keyCodeDebit);
+    }
+}
+
+var keyCodeCredit = '';
+function keypadCredit(){
+    var lastKey = $(this).text();
+    if (lastKey === 'Clear'){
+        $('.keypadDisplay').text($('.keypadDisplay').text().substring(0,$('.keypadDisplay').text().length-1));
+    } else if (lastKey === 'Enter'){
+        if (keyCodeDebit.length === 5){
+            $('.keypadDisplay').text('');
+            displayStage++;
+            changeDisplay();
+        }
+    } else {
+        keyCodeDebit += $(this).text();
+        $('.keypadDisplay').text($('.keypadDisplay').text()+'*');
+        console.log(keyCodeDebit);
     }
 }
 
 if (paymentMethod === 'credit'){
     $('.primaryDisplayContainer').text('Please enter zip code');
+    $('.keypadContainer').on('click','.row button',keypadCredit);
 } else if (paymentMethod === 'debit'){
     $('.primaryDisplayContainer').text('Please enter pin');
+    $('.keypadContainer').on('click','.row button',keypadDebit);
 }
 
