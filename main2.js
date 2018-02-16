43522f8ee965651658d775dcfc95f59238131d94
Nolan <nemerson17@gmail.com>
2018-02-15 20:40:24 -0800

    Click functionality and credit/debit decision

--- main2.js
+++ main2.js
@@ -4,16 +4,27 @@ function initializeApp(){
     $('.keypadContainer').on('click','.row button',keypadClick);
 }
 
+var keyCode = '';
 function keypadClick(){
-    if ()
-    var
+    var lastKey = $(this).text();
+    if (lastKey === 'Clear'){
+
+        //$('.keypadDisplay').text($('.keypadDisplay').text().splice($('.keypadDisplay').text().length-2),1);
+    } else if (lastKey === 'Enter'){
+        if (keyCode.length >= 4){
+            displayStage++;
+            changeDisplay();
+        }
+    } else {
+        keyCode += $(this).text();
+        $('.keypadDisplay').text($('.keypadDisplay').text()+'*');
+        console.log(keyCode);
+    }
 }
 
 if (paymentMethod === 'credit'){
     $('.primaryDisplayContainer').text('Please enter zip code');
 } else if (paymentMethod === 'debit'){
     $('.primaryDisplayContainer').text('Please enter pin');
-} else if (paymentMethod === 'cash'){
-    //go to next step
 }
 
